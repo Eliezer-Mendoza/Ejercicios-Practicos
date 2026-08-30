50e4d5d34d2791edc68e52350bde281401a86015
Eliezer-Mendoza <eliezercito888@gmail.com>
2026-08-30 16:00:10 -0600

    Auto-update: Nueva practica o ejercicio agregado

--- 04AgostoEjercicio02/Ejercici04ags/obj/Debug/net10.0/Ejercici04ags.AssemblyInfo.cs
+++ 04AgostoEjercicio02/Ejercici04ags/obj/Debug/net10.0/Ejercici04ags.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercici04ags")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5750a45c7123a74dc20c96610bc8d0d2e42beb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e190ae8e13a60c598dabcad5950cdea85bea7239")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercici04ags")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercici04ags")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 07AgostoEjercicio04/obj/Debug/net10.0/07AgostoEjercicio04.AssemblyInfo.cs
+++ 07AgostoEjercicio04/obj/Debug/net10.0/07AgostoEjercicio04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("07AgostoEjercicio04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5750a45c7123a74dc20c96610bc8d0d2e42beb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e190ae8e13a60c598dabcad5950cdea85bea7239")]
 [assembly: System.Reflection.AssemblyProductAttribute("07AgostoEjercicio04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("07AgostoEjercicio04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
