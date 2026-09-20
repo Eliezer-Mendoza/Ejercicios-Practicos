79c7023b004ddabad8efab738035c0994e72cea7
Eliezer-Mendoza <eliezercito888@gmail.com>
2026-09-20 16:00:03 -0600

    Auto-update: Nueva practica o ejercicio agregado

--- !01 General C##/PracticaIndependiente28/Program.cs
+++ !01 General C##/PracticaIndependiente28/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// en lugar de escribir todo un codigo, existen los snippets, los cuales son GoD pero god, mira:
+namespace Nfjskjdka
+{
+    class Princiapl
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine();
+        }
+    }
+}
